53f4933dbdf9fe1537116a2431d77cef7d566dab
Hyago Souza <hyago.souza0103@gmail.com>
2025-04-03 14:56:39 -0300

    Update page.js

--- app/users/page.js
+++ app/users/page.js
@@ -10,7 +10,7 @@ export default function Sla(){
     
     async function listUsers() {
         const response = await axios.get("http://localhost:3001/api/users");
-        console.log(response);
+        setUsers(response.data);
     }
 
 
@@ -23,6 +23,18 @@ export default function Sla(){
         <div>
             <h1 className="p-10 text-center text-indigo-700 bg-indigo-50 text-2xl">testando api usuarios</h1>
             <h2 className="p-10">Só testes lindos!!!</h2>
+
+
+            <h3>Estes são os usuários: </h3>
+            <hr />
+            
+            <div>
+                {
+                    users.map((i,index) => 
+                        <p className="border w-100" key={index}>{`este é o usuário: ${i.name}`} <br />{` idade: ${i.idade}`}</p>
+                    )
+                }
+            </div>
         </div>
     )
 
